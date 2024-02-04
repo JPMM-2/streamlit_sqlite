b3cab0bbf8cb0ecdd2af768f55be7b1c4306ef75
JPMM <84038900+JPMM-2@users.noreply.github.com>
2024-02-04 09:52:26 +0100

    Update py.py

--- py.py
+++ py.py
@@ -3,22 +3,44 @@ from sqlite3 import Error
 import pandas as pd
 import streamlit as st
 
-
 db_file='northwind.db'
 conn = sqlite3.connect(db_file)
 cur = conn.cursor()
 
-sql_str = "SELECT name FROM sqlite_master where type = 'table'"
-curr=cur.execute(sql_str)
-df=pd.DataFrame(curr)
-lst = df[0].tolist()
+st.write('Below is a DataFrame:')
 
-tbl = st.sidebar.selectbox('Select the table', lst)
-sql_str     = 'select * from ' + str(tbl) 
+name = st.sidebar.text_input('mete tu nombre')
+password = st.sidebar.text_input('mete tu contrasena')
 
-curr=cur.execute(sql_str)
-df=pd.DataFrame(curr)
 
-#print (lst)
 
-st.dataframe(df)
+if st.button('login'):
+
+
+	sql_str = "select * from users where name = '" + str(name) + "' and password = " + str(password)
+
+	print(sql_str)
+
+	curr=cur.execute(sql_str)
+	df=pd.DataFrame(curr)
+
+
+	if df.shape[0] == 1:
+
+		sql_str = "SELECT name FROM sqlite_master where type = 'table'"
+		curr=cur.execute(sql_str)
+		df=pd.DataFrame(curr)
+		lst = df[0].tolist()
+
+		tbl = st.sidebar.selectbox('Select the table', lst)
+		sql_str     = 'select * from ' + str(tbl) 
+
+		curr=cur.execute(sql_str)
+		df=pd.DataFrame(curr)
+
+		#print (lst)
+
+		st.dataframe(df)
+		
+	elif df.shape[0] == 0:
+		st.sidebar.write('you do not have access to the tool')
