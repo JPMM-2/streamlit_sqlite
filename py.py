3f6850b6a15be017a76c7f59d7edce2e3a3648ff
JPMM <84038900+JPMM-2@users.noreply.github.com>
2024-02-04 07:13:24 +0100

    Update py.py

--- py.py
+++ py.py
@@ -4,21 +4,6 @@ import pandas as pd
 import streamlit as st
 
 
-list_tbl = ['Territories','Suppliers','Employees','EmployeeTerritories']
-
-def create_connection(db_file):
-    """ create a database connection to a SQLite database """
-    conn = None
-    try:
-        conn = sqlite3.connect(db_file)
-        print(sqlite3.version)
-    except Error as e:
-        print(e)
-    finally:
-        if conn:
-            conn.close()
-            
-
 db_file='northwind.db'
 conn = sqlite3.connect(db_file)
 cur = conn.cursor()
@@ -26,14 +11,14 @@ cur = conn.cursor()
 sql_str = "SELECT name FROM sqlite_master where type = 'table'"
 curr=cur.execute(sql_str)
 df=pd.DataFrame(curr)
-lst = list(df)
+lst = df[0].tolist()
 
-tbl = st.sidebar.selectbox('Select the table', df[0].tolist())
+tbl = st.sidebar.selectbox('Select the table', lst)
 sql_str     = 'select * from ' + str(tbl) 
 
 curr=cur.execute(sql_str)
 df=pd.DataFrame(curr)
 
-#print ()
+#print (lst)
 
 st.dataframe(df)
